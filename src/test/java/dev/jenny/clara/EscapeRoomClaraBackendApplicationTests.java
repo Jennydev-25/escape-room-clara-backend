f0fdd16019f219e29df19c8395ea21e75d225975
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-09-14 17:16:23 +0200

    build: add initial Spring Boot project structure and dependencies

---
+++ src/test/java/dev/jenny/clara/EscapeRoomClaraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jenny.clara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeRoomClaraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
